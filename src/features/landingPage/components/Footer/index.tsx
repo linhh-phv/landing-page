aa905e90a5b064f785a89ce52af97dbe7205fab3
linhh-phv <linhh.phv@gmail.com>
2024-03-29 22:23:03 +0700

    chore: update icon and name page

--- src/features/landingPage/components/Footer/index.tsx
+++ src/features/landingPage/components/Footer/index.tsx
@@ -20,9 +20,9 @@ const Footer: React.FC<IProps> = ({}) => {
               >
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-              <p>036-221-7421</p>
+              <p>035-592-1816</p>
             </a>
-            <a href="mailto:linhh.phv@gmail.com" className="flex mx-2">
+            <a href="mailto:hoatienoc@gmail.com" className="flex mx-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6 text-red-500 mr-4"
@@ -32,12 +32,12 @@ const Footer: React.FC<IProps> = ({}) => {
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
               </svg>
-              <p>linhh.phv@gmail.com</p>
+              <p>hoatienoc@gmail.com</p>
             </a>
           </div>
         </div>
         <div className="text-center my-4">
-          <p className="text-gray-500">Copy &copy; 2024 Linh Pham</p>
+          <p className="text-gray-500">Copy &copy; 2024 Hoa Tien Oc</p>
         </div>
       </div>
     </footer>
