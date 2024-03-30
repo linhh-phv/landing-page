8dea28253c13ebd946d31878c47b0e77206f0e19
hoatienoc <165471168+hoatienoc@users.noreply.github.com>
2024-03-31 01:18:27 +0700

    chore: Update fakeData.ts

--- src/features/hoatienocPage/data/fakeData.ts
+++ src/features/hoatienocPage/data/fakeData.ts
@@ -27,46 +27,6 @@ const productsDataFake: IProduct[] = [
     link: "https://shope.ee/7UtIXFCzRM",
   },
   {
-    id: 23,
-    name: "RECHIC Đầm ngắn trễ vai Sivia đính nơ vai tay dài bồng bềnh sang trọng tiểu thư",
-    typeId: 1,
-    image:
-      "https://scontent-hkg1-2.xx.fbcdn.net/v/t39.30808-6/428600316_4059081737651433_4627098738903199479_n.jpg?stp=cp6_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Jkfl2LtG0LAAX-ZkR6S&_nc_ht=scontent-hkg1-2.xx&oh=00_AfBZLCACIamcV2YbYxDlGC0hVMmb3Rz_x2nn8lMWej1--w&oe=660B523E",
-    link: "https://www.facebook.com/hoatien.oc.9",
-  },
-  {
-    id: 3,
-    name: "RECHIC Đầm ngắn trễ vai Sivia đính nơ vai tay dài bồng bềnh sang trọng tiểu thư",
-    typeId: 2,
-    image:
-      "https://scontent-hkg1-2.xx.fbcdn.net/v/t39.30808-6/428600316_4059081737651433_4627098738903199479_n.jpg?stp=cp6_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Jkfl2LtG0LAAX-ZkR6S&_nc_ht=scontent-hkg1-2.xx&oh=00_AfBZLCACIamcV2YbYxDlGC0hVMmb3Rz_x2nn8lMWej1--w&oe=660B523E",
-    link: "https://www.facebook.com/hoatien.oc.9",
-  },
-  {
-    id: 4,
-    name: "RECHIC Đầm ngắn trễ vai Sivia đính nơ vai tay dài bồng bềnh sang trọng tiểu thư",
-    typeId: 2,
-    image:
-      "https://scontent-hkg1-2.xx.fbcdn.net/v/t39.30808-6/428600316_4059081737651433_4627098738903199479_n.jpg?stp=cp6_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Jkfl2LtG0LAAX-ZkR6S&_nc_ht=scontent-hkg1-2.xx&oh=00_AfBZLCACIamcV2YbYxDlGC0hVMmb3Rz_x2nn8lMWej1--w&oe=660B523E",
-    link: "https://www.facebook.com/hoatien.oc.9",
-  },
-  {
-    id: 5,
-    name: "Giày thể thao",
-    typeId: 3,
-    image:
-      "https://scontent-hkg1-2.xx.fbcdn.net/v/t39.30808-6/428600316_4059081737651433_4627098738903199479_n.jpg?stp=cp6_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Jkfl2LtG0LAAX-ZkR6S&_nc_ht=scontent-hkg1-2.xx&oh=00_AfBZLCACIamcV2YbYxDlGC0hVMmb3Rz_x2nn8lMWej1--w&oe=660B523E",
-    link: "https://www.facebook.com/hoatien.oc.9",
-  },
-  {
-    id: 6,
-    name: "Giày lười",
-    typeId: 3,
-    image:
-      "https://scontent-hkg1-2.xx.fbcdn.net/v/t39.30808-6/428600316_4059081737651433_4627098738903199479_n.jpg?stp=cp6_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Jkfl2LtG0LAAX-ZkR6S&_nc_ht=scontent-hkg1-2.xx&oh=00_AfBZLCACIamcV2YbYxDlGC0hVMmb3Rz_x2nn8lMWej1--w&oe=660B523E",
-    link: "https://www.facebook.com/hoatien.oc.9",
-  },
-  {
    id: 7,
     name: "Candy Skirt (có lót quần cùng chất liệu)",
     typeId: 4,
